3a0e158c1f2e48bd64e62cf52517ef9b87a72f36
thihansi <thihansig@gmail.com>
2025-10-08 21:56:42 +0530

    Add activities and layout files for Finalize, Operator Dashboard, and Scan QR functionalities

--- MobileInterface/app/src/main/java/lk/ead/mobileinterface/api/ApiClient.java
+++ MobileInterface/app/src/main/java/lk/ead/mobileinterface/api/ApiClient.java
@@ -1,4 +1,6 @@
 package lk.ead.mobileinterface.api;
 
 public class ApiClient {
+    private static final String BASE_URL =
+            "https://it223-hvhwgha5b2h5gbcg.southindia-01.azurewebsites.net/";
 }
