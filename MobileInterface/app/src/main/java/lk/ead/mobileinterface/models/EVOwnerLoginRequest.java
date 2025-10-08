1bd173d8053b774d1ebb07ce78a57050d052f5b9
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-10-08 23:29:45 +0530

    EV Operator Login feature added

--- MobileInterface/app/src/main/java/lk/ead/mobileinterface/models/EVOwnerLoginRequest.java
+++ MobileInterface/app/src/main/java/lk/ead/mobileinterface/models/EVOwnerLoginRequest.java
@@ -4,6 +4,7 @@ public class EVOwnerLoginRequest {
     private String nic;
     private String password;
 
+
     public EVOwnerLoginRequest() {}
     public EVOwnerLoginRequest(String nic, String password) {
         this.nic = nic; this.password = password;
